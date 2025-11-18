fb18f8529a8c4bba24bc3b52fddb6a45980ac283
sanhith9 <sanhithreddy9014@gmail.com>
2025-11-18 22:51:22 +0530

    Modified Sum

--- totalsum.py
+++ totalsum.py
@@ -1,8 +1,2 @@
 n = 25
-sum = 0
-average = 0
-for i in range(1,n,2):
-    sum += i
-    average = sum/n
-print(f'Total Sum is {sum}')
-print(f'Average is {average}')
+print(sum(i for i in range(1,25,2)))
